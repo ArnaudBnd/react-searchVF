bdbafb36e9736a43abcf559b352a90f6de8c1d06
arnaud <arnaudbenede@hotmail.fr>
2018-09-26 12:09:21 +0200

    https://github.com/ArnaudBnd/React-Search.git

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Search from './search'
+
+const App = () => (
+  <Search />
+)
+
+ReactDOM.render(<App />, document.getElementById('app'))
